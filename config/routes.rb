742990fa9f28c1d59027fbefe277cb959196cd9d
ichiroc <nobitahouse@gmail.com>
2026-02-06 18:59:58 +0900

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'appointments#index'
+
   resources :appointments, only: [:index, :new, :create, :show]
 
   get "up" => "rails/health#show", as: :rails_health_check
